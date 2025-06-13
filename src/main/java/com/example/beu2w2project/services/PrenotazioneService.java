89436d366defa0040ffc7984a6f63cf3f9166806
LucioGiocondo98 <togaluc@hotmail.com>
2025-06-13 17:43:24 +0200

    Update PrenotazioneService.java

--- src/main/java/com/example/beu2w2project/services/PrenotazioneService.java
+++ src/main/java/com/example/beu2w2project/services/PrenotazioneService.java
@@ -1,4 +1,13 @@
 package com.example.beu2w2project.services;
 
+import com.example.beu2w2project.repositories.PrenotazioneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PrenotazioneService {
+    @Autowired
+    private PrenotazioneRepository prenotazioneRepo;
+    @Autowired
+    DipendenteService d
 }
