0388105c7fdf861d71670d149aaa72d4ca1e44bd
LucioGiocondo98 <togaluc@hotmail.com>
2025-06-13 17:48:24 +0200

    ok

--- src/main/java/com/example/beu2w2project/services/PrenotazioneService.java
+++ src/main/java/com/example/beu2w2project/services/PrenotazioneService.java
@@ -1,5 +1,9 @@
 package com.example.beu2w2project.services;
 
+import com.example.beu2w2project.dtos.PrenotazioneDTO;
+import com.example.beu2w2project.entities.Dipendente;
+import com.example.beu2w2project.entities.Prenotazione;
+import com.example.beu2w2project.entities.Viaggio;
 import com.example.beu2w2project.repositories.PrenotazioneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +13,12 @@ public class PrenotazioneService {
     @Autowired
     private PrenotazioneRepository prenotazioneRepo;
     @Autowired
-    DipendenteService d
+    private DipendenteServices dipendenteServ;
+    @Autowired
+    private ViaggioService viaggioServ;
+
+    public Prenotazione creaPrenotazione(PrenotazioneDTO prenotazioneDTO){
+        //Dipendente dipendente=dipendenteServ.findById(prenotazioneDTO.getDipendenteId());
+        //Viaggio viaggio= viaggioServ.findById(prenotazioneDTO)
+    }
 }
